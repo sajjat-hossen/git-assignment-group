725dd777a97c06d0972be800306d5920b678218d
Sajjat004 <sajjat18cse004@gmai.com>
2024-04-29 12:09:07 +0600

    modiy divition file

--- div.h
+++ div.h
@@ -1,3 +1,45 @@
-double division(double number1, double number2) {
-  return number1 / number2;
+void division(double num1, double num2) {
+    //num1
+    if(num1 % 2 == 0){
+        cout << "(Even; ";
+        if(num1 >= 0){
+            cout << "Positive;) / ";
+        }
+        else cout << "Negative;) / ";
+    } else {
+        cout << "(Odd; ";
+        if(num1 >= 0){
+            cout << "Positive;) / ";
+        }
+        else cout << "Negative;) / ";
+    }
+ //num2
+   if(num2 % 2 == 0){
+        cout << "(Even; ";
+        if(num2 >= 0){
+            cout << "Positive;) = ";
+        }
+        else cout << "Negative;) = ";
+    } else {
+        cout << "(Odd; ";
+        if(num2 >= 0){
+            cout << "Positive;) = ";
+        }
+        else cout << "Negative;) = ";
+    }
+    //num1*num2
+     double ans = num1 / num2;
+    if(ans % 2 == 0){
+        cout << "(Even; ";
+        if(ans >= 0){
+            cout << "Positive;)";
+        }
+        else cout << "Negative;)";
+    } else {
+        cout << "(Odd; ";
+        if(ans >= 0){
+            cout << "Positive;)";
+        }
+        else cout << "Negative;)";
+    }
 }
