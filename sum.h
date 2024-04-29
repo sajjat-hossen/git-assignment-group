c4dcb4c03ebab595f4aea2853838129c721f1e61
Fuad7161 <fuadul202@gmail.com>
2024-04-29 12:14:19 +0600

    new feature added for new reales

--- sum.h
+++ sum.h
@@ -1,3 +1,45 @@
 int sum(int a,int b){
-    return a+b;
+    //num1
+    if(num1 % 2 == 0){
+        cout << "(Even; ";
+        if(num1 >= 0){
+            cout << "Positive;) + ";
+        }
+        else cout << "Negative;) + ";
+    } else {
+        cout << "(Odd; ";
+        if(num1 >= 0){
+            cout << "Positive;) + ";
+        }
+        else cout << "Negative;) + ";
+    }
+ //num2
+   if(num2 % 2 == 0){
+        cout << "(Even; ";
+        if(num2 >= 0){
+            cout << "Positive;) = ";
+        }
+        else cout << "Negative;) = ";
+    } else {
+        cout << "(Odd; ";
+        if(num2 >= 0){
+            cout << "Positive;) = ";
+        }
+        else cout << "Negative;) = ";
+    }
+    //num1*num2
+     double ans = num1 + num2;
+    if(ans % 2 == 0){
+        cout << "(Even; ";
+        if(ans >= 0){
+            cout << "Positive;)";
+        }
+        else cout << "Negative;)";
+    } else {
+        cout << "(Odd; ";
+        if(ans >= 0){
+            cout << "Positive;)";
+        }
+        else cout << "Negative;)";
+    }
 }
